60c7e4bc658e6645e0e25664cd0bda15696cd2ac
Jaskaran <thecodersingh@gmail.com>
2025-07-27 02:24:00 -0700

    Add gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/jaskaran-connects-code/',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
+    mode === 'production' &&
     componentTagger(),
   ].filter(Boolean),
   resolve: {
